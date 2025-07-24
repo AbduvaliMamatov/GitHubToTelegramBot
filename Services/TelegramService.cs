6678dee9c29b48af22e766e4466f1ccde9fa2efa
Abduvali Mamatov <thebestlittleprince@gmail.com>
2025-07-24 10:23:21 +0500

    fix: add IConfiguration and missing using

--- Services/TelegramService.cs
+++ Services/TelegramService.cs
@@ -2,6 +2,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace GitHubToTelegramBot.Services;
 
